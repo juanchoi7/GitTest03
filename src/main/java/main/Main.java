d046c4174cd8ded987d417841911e9038ac16f86
juanchoher03 <johnmail@gmail.com>
2022-09-05 23:44:38 -0300

    Comentario 5 en main varios cambios

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,12 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
         // comnetario aca
-        // comentario 2
+        // comentario EX 2
+        // comentario 4 en master y en conflicto
 
         ArrayList<Producto> productos = new ArrayList<>();
         productos.add(new Producto("nombre1", 1.0));
         productos.add(new Producto("nombre2", 2.0));
-        productos.add(new Producto("nombre3", 3.0));
+        productos.add(new Producto("nombre33", 3.0));
         productos.add(new ProductoPerecedero(7));
         productos.add(new ProductoNoPerecedero("Tipo5"));
 
@@ -24,6 +25,7 @@ public class Main {
         System.out.println(productos.get(0).calcular(6));
         System.out.println(productos.get(1).calcular(6));
         System.out.println(productos.get(2).calcular(6));
+
 
 
         System.out.println("Fin del mundo.");
